7af654922d811a0d3a0225d8bd41a95b462b8906
혀느현스 <hyuns@hyuns.dev>
2023-07-24 21:43:33 +0900

    feat: Dev Module 모듈 추가

--- src/modules/dev/dev.controller.ts
+++ src/modules/dev/dev.controller.ts
@@ -1,7 +1,14 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
 import { DevService } from './dev.service';
 
 @Controller('dev')
 export class DevController {
   constructor(private readonly devService: DevService) {}
+
+  @Get()
+  getDev() {
+    return {
+      message: 'Hello World!',
+    };
+  }
 }
--- src/modules/dev/dev.module.ts
+++ src/modules/dev/dev.module.ts
@@ -2,11 +2,11 @@ import { Module } from '@nestjs/common';
 import { DevController } from './dev.controller';
 import { DevService } from './dev.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { UserEntity } from '@/entity';
+import { PermissionEntity, UserEntity } from '@/entity';
 
 @Module({
   controllers: [DevController],
   providers: [DevService],
-  imports: [TypeOrmModule.forFeature([UserEntity])],
+  imports: [TypeOrmModule.forFeature([UserEntity, PermissionEntity])],
 })
 export class DevModule {}
