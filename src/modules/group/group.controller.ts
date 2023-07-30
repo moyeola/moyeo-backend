d81fde9cb8f4ca5bd0b06f5702b838d2209fdf4d
혀느현스 <hyuns@hyuns.dev>
2023-07-30 15:59:58 +0900

    feat: GET /groups/:groupId

--- src/modules/group/group.controller.ts
+++ src/modules/group/group.controller.ts
@@ -1,7 +1,16 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
 import { GroupService } from './group.service';
+import { GetGroupRes } from 'moyeo-object';
 
 @Controller('groups')
 export class GroupController {
   constructor(private readonly groupService: GroupService) {}
+
+  @Get(':groupId')
+  async getGroup(@Param('groupId') groupId: string): Promise<GetGroupRes> {
+    const group = await this.groupService.getGroup(parseInt(groupId));
+    return {
+      group,
+    };
+  }
 }
--- src/modules/group/group.service.ts
+++ src/modules/group/group.service.ts
@@ -1,6 +1,25 @@
+import { GroupEntity } from '@/entity';
+import { GroupObject } from '@/object';
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
 
 @Injectable()
 export class GroupService {
-  constructor() {}
+  constructor(
+    @InjectRepository(GroupEntity)
+    private readonly groupRepository: Repository<GroupEntity>,
+  ) {}
+
+  async getGroup(groupId: number) {
+    const group = await this.groupRepository.findOne({
+      where: {
+        id: groupId,
+      },
+      relations: ['members'],
+    });
+
+    const groupDto = GroupObject.from(group);
+    return groupDto;
+  }
 }
